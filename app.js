a40c3a3faff8bae6099686d53938b5c57342c986
Kallol Mukherjee <mukherjeekallol42@gmail.com>
2025-10-12 14:22:56 +0530

    phase 1 done

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ require('dotenv').config();
 const path = require("path"); //for ejs
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const expressError = require("./utils/expressError.js");
+const {listingSchema} = require("./schema.js");
 
 //now i am requiring the model from models folder
 const Listing = require("./models/listing.js")
@@ -35,12 +38,22 @@ app.get("/", (req,res)=>{
     res.send("hi, i am root page");
 });
 
+//function using joi for listing validation
+const validateListing = (req,res,next)=>{
+    let {error} = listingSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el)=>el.message).join(",");
+        throw new expressError(400,errMsg);
+    }else{
+        next();
+    }
+}
 
 //index route
-app.get("/listings" , async(req,res)=>{
+app.get("/listings" , wrapAsync(async(req,res)=>{
     const allListings = await Listing.find({}); //this listing.find() means this is the method to call the documents in the data via the model
     res.render("./listings/index.ejs" , {allListings});
-});
+}));
 
 
 //new route
@@ -49,18 +62,18 @@ app.get("/listings/new" , (req,res)=>{
 }) //new is written after show route but put upper bcs if not put upper, then the app would consider new as id, and throw err
 
 //show route
-app.get("/listings/:id" , async (req,res)=> {
+app.get("/listings/:id" ,wrapAsync( async (req,res)=> {
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("./listings/show.ejs" , {listing});
-})
+}))
 
 //create route
-app.post("/listings" ,async (req,res)=>{ //this is async func bcs we are adding in the db
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-})
+app.post("/listings" , validateListing, wrapAsync(async (req,res)=>{ //this is async func bcs we are adding in the db
+        const newListing = new Listing(req.body.listing);
+        await newListing.save();
+        res.redirect("/listings");
+}))
 
 //edit route
 app.get("/listings/:id/edit", async (req,res)=>{
@@ -70,18 +83,18 @@ app.get("/listings/:id/edit", async (req,res)=>{
 })
 
 //update route
-app.put("/listings/:id" ,async (req,res)=>{
+app.put("/listings/:id" , validateListing , async (req,res)=>{
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect("/listings");
 })
 
 //delete route
-app.delete("/listings/:id", async(req,res)=> {
+app.delete("/listings/:id", wrapAsync(async(req,res)=> {
     let {id} = req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
-})
+}))
 
 //testing the listing model
 // app.get("/testListing", async (req,res)=>{
@@ -98,6 +111,18 @@ app.delete("/listings/:id", async(req,res)=> {
 //     res.send("successful");
 
 // })
+
+
+app.use( (req,res,next)=>{
+    next(new expressError(404 , "Page Not Found!"));
+})
+
+//error-handling
+app.use((err,req,res,next)=>{
+    let {statusCode = 500, message = "Something went wrong!"} = err;
+    // res.status(statusCode).send(message);
+    res.status(statusCode).render("error.ejs" ,{err});
+})
 
 //we are starting our server at port 8080
 app.listen(8080,()=>{
