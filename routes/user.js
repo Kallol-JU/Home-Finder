49f2a053493374e2bb819b30d7308f9737513774
Kallol Mukherjee <mukherjeekallol42@gmail.com>
2025-10-27 13:58:44 +0530

    Add home route: redirect root URL to listings index page

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,11 @@ const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 const userController = require("../controllers/users.js");
 
+// Home route - redirect to listings index
+router.get("/", (req, res) => {
+    res.redirect("/listings");
+});
+
 router
 .route("/signup")
 .get(userController.renderSignup) //get signup
