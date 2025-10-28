eb89934fc6cbe8e4dd46ae57dd8f25596b1cd671
Kallol Mukherjee <mukherjeekallol42@gmail.com>
2025-10-29 01:11:47 +0530

    done the filters live working part

--- controllers/listings.js
+++ controllers/listings.js
@@ -4,10 +4,12 @@ const mapKey = process.env.MAP_TOKEN;
 
 
 module.exports.index = async (req, res) => {
-    const { q } = req.query;
-    let filter = {};
+    const { q, filter } = req.query;
+    let searchFilter = {};
+    
+    // Handle text search
     if (q) {
-        filter = {
+        searchFilter = {
             // '$or' searches multiple fields
             $or: [
                 // Checking if 'q' is in the title/country/location
@@ -16,8 +18,92 @@ module.exports.index = async (req, res) => {
                 { location: { $regex: q, $options: "i" } }
             ]
         };
-    };
-    const allListings = await Listing.find(filter);
+    }
+    
+    // Handle category filters
+    if (filter) {
+        switch (filter) {
+            case 'trending':
+                // For trending, we'll show listings with most reviews (most popular)
+                const trendingListings = await Listing.find(searchFilter).populate('reviews');
+                const sortedListings = trendingListings.sort((a, b) => b.reviews.length - a.reviews.length);
+                return res.render("listings/index.ejs", { allListings: sortedListings });
+                
+            case 'rooms':
+                searchFilter.$or = [
+                    { title: { $regex: /room|bedroom|suite|apartment/i } },
+                    { description: { $regex: /room|bedroom|suite|apartment/i } }
+                ];
+                break;
+                
+            case 'iconic-cities':
+                searchFilter.$or = [
+                    { location: { $regex: /paris|london|new york|tokyo|rome|barcelona|amsterdam|prague|vienna|budapest/i } },
+                    { country: { $regex: /france|england|usa|japan|italy|spain|netherlands|czech|austria|hungary/i } }
+                ];
+                break;
+                
+            case 'mountains':
+                searchFilter.$or = [
+                    { title: { $regex: /mountain|alpine|peak|summit|hill/i } },
+                    { description: { $regex: /mountain|alpine|peak|summit|hill/i } },
+                    { location: { $regex: /mountain|alpine|peak|summit|hill/i } }
+                ];
+                break;
+                
+            case 'castles':
+                searchFilter.$or = [
+                    { title: { $regex: /castle|palace|fortress|chateau|manor/i } },
+                    { description: { $regex: /castle|palace|fortress|chateau|manor/i } }
+                ];
+                break;
+                
+            case 'amazing-pools':
+                searchFilter.$or = [
+                    { title: { $regex: /pool|swimming|aqua|water/i } },
+                    { description: { $regex: /pool|swimming|aqua|water|infinity pool|jacuzzi/i } }
+                ];
+                break;
+                
+            case 'camping':
+                searchFilter.$or = [
+                    { title: { $regex: /camp|camping|tent|cabin|lodge/i } },
+                    { description: { $regex: /camp|camping|tent|cabin|lodge|outdoor/i } }
+                ];
+                break;
+                
+            case 'farms':
+                searchFilter.$or = [
+                    { title: { $regex: /farm|ranch|barn|countryside|rural/i } },
+                    { description: { $regex: /farm|ranch|barn|countryside|rural|agriculture/i } }
+                ];
+                break;
+                
+            case 'arctic':
+                searchFilter.$or = [
+                    { title: { $regex: /arctic|ice|snow|northern|aurora/i } },
+                    { description: { $regex: /arctic|ice|snow|northern|aurora|winter/i } },
+                    { location: { $regex: /arctic|ice|snow|northern/i } }
+                ];
+                break;
+                
+            case 'bed-and-breakfast':
+                searchFilter.$or = [
+                    { title: { $regex: /bed and breakfast|b&b|breakfast|inn/i } },
+                    { description: { $regex: /bed and breakfast|b&b|breakfast|inn|homestay/i } }
+                ];
+                break;
+                
+            case 'domes':
+                searchFilter.$or = [
+                    { title: { $regex: /dome|geodesic|spherical|bubble/i } },
+                    { description: { $regex: /dome|geodesic|spherical|bubble|glamping/i } }
+                ];
+                break;
+        }
+    }
+    
+    const allListings = await Listing.find(searchFilter);
     res.render("listings/index.ejs", { allListings });
 };
 
